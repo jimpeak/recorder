eade18186245e90bb0a1c66becce462af0e2663c
Andre Paquet <andre.paquet@gmail.com>
2016-03-07 23:47:34 -0500

    Move types to library

--- app/Main.hs
+++ app/Main.hs
@@ -15,55 +15,6 @@ import           Database.MySQL.Simple
 import           Options.Generic
 import           Prelude               hiding (putStr, putStrLn, unwords)
 
-data SqlType = SqlText | SqlInt | SqlLong | SqlBool | SqlNumeric | SqlByteArray | SqlDate | SqlTime | SqlOther | SqlMaybe SqlType
-
-data HaskellType = HaskText | HaskInt | HaskLong | HaskBool | HaskNumeric | HaskByteArray | HaskDate | HaskTime | HaskUnknown | HaskMaybe HaskellType
-
-data Table = Table {
-    tname :: Text,
-    cols  :: [Column]
-}
-
-data Column = Column {
-    cname :: Text,
-    ctyp  :: SqlType,
-    dflt  :: Maybe Text,
-    iskey :: Bool
-}
-
-data Record = Record {
-    hname  :: Text,
-    fields :: [Field]
-}
-
-data Field = Field {
-    fname :: Text,
-    ftyp  :: HaskellType,
-    fiskey :: Bool
-}
-
-haskFromSql SqlText = HaskText
-haskFromSql SqlInt = HaskInt
-haskFromSql SqlLong = HaskLong
-haskFromSql SqlBool = HaskBool
-haskFromSql SqlNumeric = HaskNumeric
-haskFromSql SqlByteArray = HaskByteArray
-haskFromSql SqlDate = HaskDate
-haskFromSql SqlTime = HaskTime
-haskFromSql SqlOther = HaskUnknown
-haskFromSql (SqlMaybe t) = HaskMaybe $ haskFromSql t
-
-typeToStr HaskText = "!Text"
-typeToStr HaskInt = "!Int"
-typeToStr HaskLong = "!Integer"
-typeToStr HaskBool = "!Bool"
-typeToStr HaskNumeric = "!Decimal"
-typeToStr HaskByteArray = "!ByteArray"
-typeToStr HaskDate = "!Day"
-typeToStr HaskTime = "!UTCTime"
-typeToStr HaskUnknown = "!Unknown"
-typeToStr (HaskMaybe t) = append "!Maybe " $ typeToStr t
-
 convertSqlType s isnull
       | isnull == "YES" = SqlMaybe $ convertSqlType s ""
       | s `elem` ["text", "varchar", "char"] = SqlText
--- src/Data/Recorder.hs
+++ src/Data/Recorder.hs
@@ -1,10 +1,69 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, OverloadedStrings #-}
 
 module Data.Recorder (
-    Recorder
+    Recorder,
+    SqlType(..),
+    HaskellType(..),
+    Record(..),
+    Column(..),
+    Field(..),
+    Table(..),
+    haskFromSql,
+    typeToStr
 ) where
 
 import Database.MySQL.Simple
+import Data.Text
+
+data SqlType = SqlText | SqlInt | SqlLong | SqlBool | SqlNumeric | SqlByteArray | SqlDate | SqlTime | SqlOther | SqlMaybe SqlType
+
+data HaskellType = HaskText | HaskInt | HaskLong | HaskBool | HaskNumeric | HaskByteArray | HaskDate | HaskTime | HaskUnknown | HaskMaybe HaskellType
+
+data Table = Table {
+    tname :: Text,
+    cols  :: [Column]
+}
+
+data Column = Column {
+    cname :: Text,
+    ctyp  :: SqlType,
+    dflt  :: Maybe Text,
+    iskey :: Bool
+}
+
+data Record = Record {
+    hname  :: Text,
+    fields :: [Field]
+}
+
+data Field = Field {
+    fname :: Text,
+    ftyp  :: HaskellType,
+    fiskey :: Bool
+}
+
+haskFromSql SqlText = HaskText
+haskFromSql SqlInt = HaskInt
+haskFromSql SqlLong = HaskLong
+haskFromSql SqlBool = HaskBool
+haskFromSql SqlNumeric = HaskNumeric
+haskFromSql SqlByteArray = HaskByteArray
+haskFromSql SqlDate = HaskDate
+haskFromSql SqlTime = HaskTime
+haskFromSql SqlOther = HaskUnknown
+haskFromSql (SqlMaybe t) = HaskMaybe $ haskFromSql t
+
+typeToStr HaskText = "!Text"
+typeToStr HaskInt = "!Int"
+typeToStr HaskLong = "!Integer"
+typeToStr HaskBool = "!Bool"
+typeToStr HaskNumeric = "!Decimal"
+typeToStr HaskByteArray = "!ByteArray"
+typeToStr HaskDate = "!Day"
+typeToStr HaskTime = "!UTCTime"
+typeToStr HaskUnknown = "!Unknown"
+typeToStr (HaskMaybe t) = append "!Maybe " $ typeToStr t
+
 
 class Recorder a where
     type Key a
